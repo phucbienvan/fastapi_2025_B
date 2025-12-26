639563044b140fa525eb9e7e1a3b051a352aa11a
Vanthanh30 <155293495+Vanthanh30@users.noreply.github.com>
2025-12-26 18:23:04 +0700

    user_model

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,10 +1,7 @@
 from app.models.base_model import BaseModel
 from sqlalchemy import Column, String, Float, DateTime, Integer
-<<<<<<< HEAD
 from datetime import datetime
-=======
-import datetime
->>>>>>> e6345e271988d41db266fe35c6a4de4ed9a3e48e
+
 
 class User(BaseModel):
     __tablename__ = "users"
@@ -12,11 +9,7 @@ class User(BaseModel):
     name: str = Column(String, index=True)
     email: str = Column(String, index=True)
     password: str = Column(String, index=True)
-<<<<<<< HEAD
     status: int = Column(Integer, index=True, default=1)
     created_at: datetime = Column(DateTime, index=True, default=datetime.now)
     updated_at: datetime = Column(DateTime, index=True, default=datetime.now)
-=======
-    created_at: datetime.datetime = Column(DateTime, index=True)
-    updated_at: datetime.datetime = Column(DateTime, index=True)
->>>>>>> e6345e271988d41db266fe35c6a4de4ed9a3e48e
+
